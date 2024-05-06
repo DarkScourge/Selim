0c867a98ff06c202ee9dd65f7d6dc85d7451476b
DarkScourge <167119623+DarkScourge@users.noreply.github.com>
2024-05-06 21:36:07 +0300

    Update M1L2.py

--- M1L2.py
+++ M1L2.py
@@ -1,1 +1,22 @@
-Arkadaşlar neden söylemediniz.
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.command()
+async def repeat(ctx, times: int, content='BEN LİBERALİST BİR BOTUM'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run('Token gir')
