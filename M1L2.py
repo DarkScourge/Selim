38f38ec818858a8f56ea2fd272e5030a2660ada4
DarkScourge <167119623+DarkScourge@users.noreply.github.com>
2024-05-06 21:31:44 +0300

    Update M1L2.py

--- M1L2.py
+++ M1L2.py
@@ -1,9 +1,22 @@
+import discord
+from discord.ext import commands
 import random
 
-sifre_secenekleri = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-soru = int(input("Şifrenizi uzunluğunu giriniz:"))
-parola = ""
-for i in range(soru):
-    secenek = random.choice(sifre_secenekleri)
-    parola+=secenek
-print(parola)
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.command()
+async def repeat(ctx, times: int, content='BEN LİBERALİST BİR BOTUM'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run('Token gir')
